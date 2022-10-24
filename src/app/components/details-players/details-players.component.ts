bdebf13d3110a30087bd39398895d5f358bfbe86
Mario Ruiz López <ruiz.lomar22@triana.salesianos.edu>
2022-10-24 12:45:49 +0200

    Inclusión de estadísticas por temporada

--- src/app/components/details-players/details-players.component.ts
+++ src/app/components/details-players/details-players.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { CareerSummary } from 'src/app/interfaces/player-stats.interface';
+import { ActivatedRoute } from '@angular/router';
+import { CareerSummary, Season } from 'src/app/interfaces/player-stats.interface';
 import { Draft, Player, TeamPlayed } from 'src/app/interfaces/players.interface';
 import { Team } from 'src/app/interfaces/teams.interface';
 import { PlayersService } from 'src/app/services/players.service';
@@ -14,26 +14,37 @@ import { TeamService } from 'src/app/services/team.service';
 export class DetailsPlayersComponent implements OnInit {
 
   id: string = '';
-  player: Player = {} as Player
-  summary: CareerSummary = {} as CareerSummary
+  colorPpal: string = '';
+  colorSec: string = '';
+  colorLet: string = '';
+  colorTexto: string = '';
+  player: Player = {} as Player;
+  summary: CareerSummary = {} as CareerSummary;
+  seasons: Season[] = [];
+  statsPerYear: Season[] = [];
   playerList: Player[] = [];
   teamList: Team[] = [];
   teamPlayed: string[] = [];
   teamDraft: string = '';
   yearList: number[] = [];
   year: number = 0;
-  teamURLbase1: string = 'https://cdn.nba.com/logos/nba/'
-  teamURLbase2: string = '/global/L/logo.svg'
+  yearStats: number = 0;
+  teamURLbase1: string = 'https://cdn.nba.com/logos/nba/';
+  teamURLbase2: string = '/global/L/logo.svg';
+  stats1 = false;
+  stats2 = false;
 
-  constructor(private route: ActivatedRoute, private router: Router, private playerService: PlayersService, private teamService: TeamService) { }
+  constructor(private route: ActivatedRoute, private playerService: PlayersService, private teamService: TeamService) { }
 
   ngOnInit(): void {
-    /*this.id = this.route.snapshot.paramMap.get('id')!;
-    this.year = this.route.snapshot.paramMap.get('year')!;*/
     this.route.params.subscribe(resp => {
       this.id = resp['id']
       this.year = resp['year']
     });
+
+    for (let i = 1; i < 13; i++) {
+      this.yearList.push(this.year - i);
+    }
 
     this.playerService.getPlayerList(this.year).subscribe(resp => {
       for (let player of resp.league.standard) {
@@ -41,11 +52,133 @@ export class DetailsPlayersComponent implements OnInit {
           this.player = player
         }
       }
+      if (this.player.teamId == '1610612737') {
+        this.colorPpal = 'background-color: rgb(200, 16, 46);'
+        this.colorSec = 'background-color: rgb(253, 185, 39);'
+        this.colorLet = 'color: black'
+      } else if (this.player.teamId == '1610612738') {
+        this.colorPpal = 'background-color: rgb(0, 122, 51);'
+        this.colorSec = 'background-color: rgb(139, 111, 78);'
+        this.colorLet = 'color: black'
+      } else if (this.player.teamId == '1610612751') {
+        this.colorPpal = 'background-color: rgb(0, 0, 0);'
+        this.colorSec = 'background-color: rgb(255, 255, 255);'
+        this.colorTexto = 'color: white'
+      } else if (this.player.teamId == '1610612766') {
+        this.colorPpal = 'background-color: rgb(29, 17, 96);'
+        this.colorSec = 'background-color: rgb(0, 120, 140);'
+        this.colorTexto = 'color: white'
+      } else if (this.player.teamId == '1610612741') {
+        this.colorPpal = 'background-color: rgb(206, 17, 65);'
+        this.colorSec = 'background-color: rgb(6, 25, 34);'
+        this.colorLet = 'color: white'
+      } else if (this.player.teamId == '1610612739') {
+        this.colorPpal = 'background-color: rgb(134, 0, 56);'
+        this.colorSec = 'background-color: rgb(4, 30, 66);'
+        this.colorLet = 'color: white'
+        this.colorTexto = 'color: black'
+      } else if (this.player.teamId == '1610612742') {
+        this.colorPpal = 'background-color: rgb(0, 83, 188);'
+        this.colorSec = 'background-color: rgb(0, 43, 92);'
+        this.colorLet = 'color: white'
+        this.colorTexto = 'color: black'
+      } else if (this.player.teamId == '1610612743') {
+        this.colorPpal = 'background-color: rgb(13, 34, 64);'
+        this.colorSec = 'background-color: rgb(255, 198, 39);'
+        this.colorTexto = 'color: white'
+      } else if (this.player.teamId == '1610612765') {
+        this.colorPpal = 'background-color: rgb(200,16,46);'
+        this.colorSec = 'background-color: rgb(29,66,138);'
+      } else if (this.player.teamId == '1610612744') {
+        this.colorPpal = 'background-color: rgb(29, 66, 138);'
+        this.colorSec = 'background-color: rgb(255, 199, 44);'
+      } else if (this.player.teamId == '1610612745') {
+        this.colorPpal = 'background-color: rgb(206,17,65);'
+        this.colorSec = 'background-color: rgb(6,25,34);'
+        this.colorLet = 'color: white'
+        this.colorTexto = 'color: black'
+      } else if (this.player.teamId == '1610612754') {
+        this.colorPpal = 'background-color: rgb(0, 45, 98);'
+        this.colorSec = 'background-color: rgb(253, 187, 48);'
+        this.colorTexto = 'color: white'
+      } else if (this.player.teamId == '1610612746') {
+        this.colorPpal = 'background-color: rgb(200,16,46);'
+        this.colorSec = 'background-color: rgb(29,66,148);'
+      } else if (this.player.teamId == '1610612747') {
+        this.colorPpal = 'background-color: rgb(85, 37, 130);'
+        this.colorSec = 'background-color: rgb(253, 185, 39);'
+      } else if (this.player.teamId == '1610612763') {
+        this.colorPpal = 'background-color: rgb(93, 118, 169);'
+        this.colorSec = 'background-color: rgb(18, 23, 63);'
+        this.colorLet = 'color: white'
+        this.colorTexto = 'color: black'
+      } else if (this.player.teamId == '1610612748') {
+        this.colorPpal = 'background-color: rgb(152, 0, 46);'
+        this.colorSec = 'background-color: rgb(249, 160, 27);'
+      } else if (this.player.teamId == '1610612749') {
+        this.colorPpal = 'background-color: rgb(0, 71, 27);'
+        this.colorSec = 'background-color: rgb(240, 235, 210);'
+      } else if (this.player.teamId == '1610612750') {
+        this.colorPpal = 'background-color: rgb(12, 35, 64);'
+        this.colorSec = 'background-color: rgb(35, 97, 146);'
+        this.colorTexto = 'color: white'
+      } else if (this.player.teamId == '1610612740') {
+        this.colorPpal = 'background-color: rgb(0, 22, 65);'
+        this.colorSec = 'background-color: rgb(225, 58, 62);'
+        this.colorTexto = 'color: white'
+      } else if (this.player.teamId == '1610612752') {
+        this.colorPpal = 'background-color: rgb(0, 107, 182);'
+        this.colorSec = 'background-color: rgb(245, 132, 38);'
+      } else if (this.player.teamId == '1610612760') {
+        this.colorPpal = 'background-color: rgb(0, 125, 195);'
+        this.colorSec = 'background-color: rgb(239, 59, 36);'
+      } else if (this.player.teamId == '1610612753') {
+        this.colorPpal = 'background-color: rgb(0, 125, 197);'
+        this.colorSec = 'background-color: rgb(196, 206, 211);'
+      } else if (this.player.teamId == '1610612755') {
+        this.colorPpal = 'background-color: rgb(0, 107, 182);'
+        this.colorSec = 'background-color: rgb(237, 23, 76);'
+      } else if (this.player.teamId == '1610612756') {
+        this.colorPpal = 'background-color: rgb(29, 17, 96);'
+        this.colorSec = 'background-color: rgb(229, 95, 32);'
+        this.colorTexto = 'color: white'
+      } else if (this.player.teamId == '1610612757') {
+        this.colorPpal = 'background-color: rgb(224, 58, 62);'
+        this.colorSec = 'background-color: rgb(6, 25, 34);'
+        this.colorLet = 'color: white'
+        this.colorTexto = 'color: black'
+      } else if (this.player.teamId == '1610612758') {
+        this.colorPpal = 'background-color: rgb(91,43,130);'
+        this.colorSec = 'background-color: rgb(99,113,122);'
+      } else if (this.player.teamId == '1610612759') {
+        this.colorPpal = 'background-color: rgb(196, 206, 211);'
+        this.colorSec = 'background-color: rgb(6, 25, 34);'
+        this.colorLet = 'color: white'
+        this.colorTexto = 'color: black'
+      } else if (this.player.teamId == '1610612761') {
+        this.colorPpal = 'background-color: rgb(206, 17, 65);'
+        this.colorSec = 'background-color: rgb(6, 25, 34);'
+        this.colorLet = 'color: white'
+        this.colorTexto = 'color: black'
+      } else if (this.player.teamId == '1610612762') {
+        this.colorPpal = 'background-color: rgb(0, 43, 92);'
+        this.colorSec = 'background-color: rgb(0, 71, 27);'
+        this.colorLet = 'color: white'
+        this.colorTexto = 'color: black'
+      } else if (this.player.teamId == '1610612764') {
+        this.colorPpal = 'background-color: rgb(0,43,92);'
+        this.colorSec = 'background-color: rgb(227,24,55);'
+        this.colorTexto = 'color: white'
+      }
     });
 
     this.playerService.getStats(this.year, this.id).subscribe(resp => {
       this.summary = resp.league.standard.stats.careerSummary
     });
+
+    this.playerService.getStats(this.year, this.id).subscribe(resp => {
+      this.seasons = resp.league.standard.stats.regularSeason.season
+    })
 
     this.teamService.getTeams(this.year).subscribe(resp => {
       this.teamList = resp.league.standard;
@@ -110,5 +243,25 @@ export class DetailsPlayersComponent implements OnInit {
       }
     }
     return this.teamDraft
+  }
+
+  showStats1() {
+    this.stats1 = true;
+    this.stats2 = false;
+  }
+
+  showStats2() {
+    this.stats1 = false;
+    this.stats2 = true;
+    this.statsPerYear = this.seasons;
+  }
+
+  selectYear() {
+    this.statsPerYear = [];
+    for (let season of this.seasons) {
+      if (season.seasonYear == this.yearStats) {
+        this.statsPerYear.push(season);
+      }
+    }
   }
 }
--- src/app/components/player-list/players.component.ts
+++ src/app/components/player-list/players.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Player } from 'src/app/interfaces/players.interface';
 import { Team } from 'src/app/interfaces/teams.interface';
 import { PlayersService } from 'src/app/services/players.service';
@@ -11,12 +12,17 @@ import { TeamService } from 'src/app/services/team.service';
 })
 export class PlayersComponent implements OnInit {
 
+  teams = new FormControl('');
+  colorPpal: string = '';
+  colorTexto: string = '';
+  filteredTeamList: Team[] = [];
+  filteredPlayerList: Player[] = [];
   playerList: Player[] = [];
   teamList: Team[] = [];
-  yearList: number[] = []
+  yearList: number[] = [];
   year: number = 0;
-  teamURLbase1: string = 'https://cdn.nba.com/logos/nba/'
-  teamURLbase2: string = '/global/L/logo.svg'
+  teamURLbase1: string = 'https://cdn.nba.com/logos/nba/';
+  teamURLbase2: string = '/global/L/logo.svg';
 
   constructor(private playerService: PlayersService, private teamService: TeamService) { }
 
@@ -25,6 +31,7 @@ export class PlayersComponent implements OnInit {
 
     this.playerService.getPlayerList(this.year).subscribe(resp => {
       this.playerList = resp.league.standard;
+      this.filteredPlayerList = this.playerList
     })
 
     this.teamService.getTeams(this.year).subscribe(resp => {
@@ -51,6 +58,119 @@ export class PlayersComponent implements OnInit {
         return team.fullName;
       }
     }
-    return undefined
+    return undefined;
+  }
+
+  changeList() {
+    let listAux: Player[] = [];
+    for (let player of this.playerList) {
+      for (let team of this.filteredTeamList) {
+        if (player.teamId == team.teamId) {
+          listAux.push(player);
+        }
+      }
+    }
+    if (listAux.length == 0) {
+      this.filteredPlayerList = this.playerList
+    } else {
+      this.filteredPlayerList = listAux
+    }
+  }
+
+  background(id: string) {
+    if (id == '1610612737') {
+      this.colorPpal = 'background-color: rgb(200, 16, 46);'
+    } else if (id == '1610612738') {
+      this.colorPpal = 'background-color: rgb(0, 122, 51);'
+    } else if (id == '1610612751') {
+      this.colorPpal = 'background-color: rgb(0, 0, 0);'
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612766') {
+      this.colorPpal = 'background-color: rgb(29, 17, 96);'
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612741') {
+      this.colorPpal = 'background-color: rgb(206, 17, 65);'
+    } else if (id == '1610612739') {
+      this.colorPpal = 'background-color: rgb(134, 0, 56);'
+    } else if (id == '1610612742') {
+      this.colorPpal = 'background-color: rgb(0, 83, 188);'
+    } else if (id == '1610612743') {
+      this.colorPpal = 'background-color: rgb(13, 34, 64);'
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612765') {
+      this.colorPpal = 'background-color: rgb(200,16,46);'
+    } else if (id == '1610612744') {
+      this.colorPpal = 'background-color: rgb(29, 66, 138);'
+    } else if (id == '1610612745') {
+      this.colorPpal = 'background-color: rgb(206,17,65);'
+    } else if (id == '1610612754') {
+      this.colorPpal = 'background-color: rgb(0, 45, 98);'
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612746') {
+      this.colorPpal = 'background-color: rgb(200,16,46);'
+    } else if (id == '1610612747') {
+      this.colorPpal = 'background-color: rgb(85, 37, 130);'
+    } else if (id == '1610612763') {
+      this.colorPpal = 'background-color: rgb(93, 118, 169);'
+    } else if (id == '1610612748') {
+      this.colorPpal = 'background-color: rgb(152, 0, 46);'
+    } else if (id == '1610612749') {
+      this.colorPpal = 'background-color: rgb(0, 71, 27);'
+    } else if (id == '1610612750') {
+      this.colorPpal = 'background-color: rgb(12, 35, 64);'
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612740') {
+      this.colorPpal = 'background-color: rgb(0, 22, 65);'
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612752') {
+      this.colorPpal = 'background-color: rgb(0, 107, 182);'
+    } else if (id == '1610612760') {
+      this.colorPpal = 'background-color: rgb(0, 125, 195);'
+    } else if (id == '1610612753') {
+      this.colorPpal = 'background-color: rgb(0, 125, 197);'
+    } else if (id == '1610612755') {
+      this.colorPpal = 'background-color: rgb(0, 107, 182);'
+    } else if (id == '1610612756') {
+      this.colorPpal = 'background-color: rgb(29, 17, 96);'
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612757') {
+      this.colorPpal = 'background-color: rgb(224, 58, 62);'
+    } else if (id == '1610612758') {
+      this.colorPpal = 'background-color: rgb(91,43,130);'
+    } else if (id == '1610612759') {
+      this.colorPpal = 'background-color: rgb(196, 206, 211);'
+    } else if (id == '1610612761') {
+      this.colorPpal = 'background-color: rgb(206, 17, 65);'
+    } else if (id == '1610612762') {
+      this.colorPpal = 'background-color: rgb(0, 43, 92);'
+    } else if (id == '1610612764') {
+      this.colorPpal = 'background-color: rgb(0,43,92);'
+      this.colorTexto = 'color: white'
+    }
+    return this.colorPpal
+  }
+
+  color(id: string) {
+    if (id == '1610612751') {
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612766') {
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612742') {
+    } else if (id == '1610612743') {
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612754') {
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612750') {
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612740') {
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612756') {
+      this.colorTexto = 'color: white'
+    } else if (id == '1610612764') {
+      this.colorTexto = 'color: white'
+    } else {
+      this.colorTexto = 'color: black'
+    }
+    return this.colorTexto
   }
 }
